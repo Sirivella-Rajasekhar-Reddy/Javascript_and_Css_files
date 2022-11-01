0d4afc875f63bc74d90585dad98f6e368ef7ba21
Sirivella-Rajasekhar-Reddy <105851813+Sirivella-Rajasekhar-Reddy@users.noreply.github.com>
2022-11-01 21:50:01 +0530

    first commit
    first commit

---
+++ formvalidation2.js
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <title>forms creation</title>
+    </head>
+    <body>
+        <form>
+            enter name : <input type="text" name="name" required/><br/><br/>
+            enter mail : <input type="email" name="email" required/><br/><br/>
+            enter password :<input type="password" name="pass1" required/><br/><br/>
+            enter confirm password : <input type="password" name="pass2" required/><br/><br/>
+            enter phone number : <input type="number" name="num" required/><br/><br/>
+        </form>
+    </body>
+</html>
